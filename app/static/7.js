4945302f5019c315565707774bef9caa87008322
miron6 <mirond@seclab.cs.msu.ru>
2020-12-21 12:28:34 +0300

    Add js for DEP 7

---
+++ app/static/7.js
@@ -0,0 +1,10 @@
+var param7 = "";
+
+function request7() {
+    var url = "/application/n3m1k2/interface/zgjj56/handle?lk90pj=1";
+        url = url + "&" + param7;
+    $.ajax({
+        type: "GET",
+        url: url
+    });
+}
