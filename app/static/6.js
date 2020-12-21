e70b4a2708bcd2a3c03be483127c0186d7f1de59
miron6 <mirond@seclab.cs.msu.ru>
2020-12-21 12:23:49 +0300

    Add DEP number 6 and jQuery

---
+++ app/static/6.js
@@ -0,0 +1,9 @@
+function request6() {
+    var url = "/application/gf32d2/interface/vcj442/handle";
+    var param = "lkvo24=1";
+    $.ajax({
+        type: "GET",
+        url: url,
+        data: param
+    });
+}
